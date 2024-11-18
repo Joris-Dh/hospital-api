41985071bffed77c9a1864bfde02b4fe3fdf3a51
Joris Dh <joris.dh@outlook.be>
2024-11-18 16:18:11 +0100

    update with GET and POST request working

--- src/main/java/com/joris/hospital_api/controller/ApiController.java
+++ src/main/java/com/joris/hospital_api/controller/ApiController.java
@@ -1,13 +1,35 @@
 package com.joris.hospital_api.controller;
 
+import com.joris.hospital_api.models.User;
+import com.joris.hospital_api.repository.userRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ApiController {
+
+    @Autowired
+    private userRepo userRepo;
 
     @GetMapping("/")
     public String getPage() {
         return "hello";
     }
+
+    @GetMapping("/users")
+    public List<User> getUsers() {
+        return userRepo.findAll();
+    }
+
+    @PostMapping(value = "/save")
+    public String saveUser(@RequestBody User user) {
+        userRepo.save(user);
+        return "Saved...";
+    }
+
 }
--- src/main/java/com/joris/hospital_api/repository/userRepo.java
+++ src/main/java/com/joris/hospital_api/repository/userRepo.java
@@ -3,5 +3,6 @@ package com.joris.hospital_api.repository;
 import com.joris.hospital_api.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepo extends JpaRepository<User, Long> {
+public interface userRepo extends JpaRepository<User, Long>{
 }
+
